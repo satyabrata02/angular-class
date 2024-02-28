efa8fc03ab84b00c89a7c408db28fbdf20fab2fe
satya <pandasatyabrata56@gmail.com>
2024-02-28 13:32:48 +0530

    updated

--- angular/lifeCycle/src/app/app.component.ts
+++ angular/lifeCycle/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'lifeCycle';
+  c = 0;
+  constructor(){
+    console.log("constructor called")
+  }
+  incr(){
+    this.c++;
+  }
 }
