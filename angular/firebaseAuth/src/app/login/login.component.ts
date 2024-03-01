49ff63e45a66565d0e0b1c5331d8a88eee2747f4
Satyabrata Panda <pandasatyabrata566@gmail.com>
2024-03-01 21:41:21 +0530

    updated

--- angular/firebaseAuth/src/app/login/login.component.ts
+++ angular/firebaseAuth/src/app/login/login.component.ts
@@ -8,13 +8,13 @@ import { Router } from '@angular/router';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  
+
   constructor(private as: AuthService, private router:Router) {}
   login(email: any, password: any) {
     this.as.login(email.control.value, password.control.value)
       .then((val) => {
         console.log(val);
-        alert("successfully logged in")
+        alert("successfully logged in");
       })
     .catch((val)=>alert("email or password not matched"))
   }
