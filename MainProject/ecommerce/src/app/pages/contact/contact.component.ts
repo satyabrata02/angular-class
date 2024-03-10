53f85d6a4c0583cab07afd7016195f5e17581dea
Satyabrata Panda <pandasatyabrata566@gmail.com>
2024-03-10 15:40:59 +0530

    updated

--- MainProject/ecommerce/src/app/pages/contact/contact.component.ts
+++ MainProject/ecommerce/src/app/pages/contact/contact.component.ts
@@ -12,4 +12,14 @@ export class ContactComponent {
   constructor(private titleService: Title){ 
     titleService.setTitle(this.title);
   }
+
+  appNumbersOnly(event: KeyboardEvent) {
+    const allowedKeys = ['Backspace', 'Delete', 'ArrowLeft', 'ArrowRight'];
+
+    if ( allowedKeys.includes(event.key) || (event.key >= '0' && event.key <= '9')) {
+      return; // Allow: backspace, delete, arrow keys, and numbers
+    } else {
+      event.preventDefault();
+    }
+  }
 }
